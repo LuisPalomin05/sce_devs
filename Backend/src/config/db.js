800217f6f020c6dd4c20a40e86fbfa3a049a6916
luispal7030 <I202224764@cibertec.edu.pe>
2026-04-07 11:58:12 -0500

    FIX

--- Backend/src/config/db.js
+++ Backend/src/config/db.js
@@ -2,11 +2,11 @@ const mysql = require("mysql2/promise");
 require("dotenv").config();
 
 const pool = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "mysql",
-  database: "SCEDATABASE_Nikolas",
-  port: 3306,
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT,
   waitForConnections: true,
   connectionLimit: 10,
 });
