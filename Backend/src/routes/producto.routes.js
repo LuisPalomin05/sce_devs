4fbf70bd64c2d420b50053ff7396406e047b7506
LuisPalomin05 <pmpalominomedina@gmail.com>
2026-04-07 20:17:55 -0500

    separacion logica para /productos

---
+++ Backend/src/routes/producto.routes.js
@@ -0,0 +1,9 @@
+const express = require('express'); 
+const router = express.Router();
+const productoController = require('../controllers/producto.controller');
+
+
+router.post('/');
+router.put('/:id');
+
+module.exports = router;
