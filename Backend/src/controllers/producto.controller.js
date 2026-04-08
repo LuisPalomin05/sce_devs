0a79ceaf4556ae74f104dd232f2570fffa9c8b1d
LuisPalomin05 <pmpalominomedina@gmail.com>
2026-04-07 20:17:55 -0500

    separacion logica para /productos

---
+++ Backend/src/controllers/producto.controller.js
@@ -0,0 +1,33 @@
+const prodRepository = require('../models/productoRepository');
+
+const getAllProductos = async (req, res)=> { 
+try {
+    
+} catch (error) {
+    
+}
+}
+
+const getProductoByID = async (req, res)=> { 
+try {
+    
+} catch (error) {
+    
+}
+}
+
+const deleteProducto = async (req, res)=> { 
+try {
+    
+} catch (error) {
+    
+}
+}
+
+const editProducto = async (req, res)=> { 
+try {
+    
+} catch (error) {
+    
+}
+}
---
+++ Backend/src/models/productoRepository.js
@@ -0,0 +1,17 @@
+const pool= require("../config/db");
+
+const createProducto = async () => {
+
+}
+
+const deleteProducto = async () => {
+
+}
+
+const updateProducto = async () =>{
+
+}
+
+const getProducto = async () =>{
+
+}
